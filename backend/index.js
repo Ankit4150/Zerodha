e6a8f01dc2d5344fc7a24ae23f6152595a635b37
ankit kushwah <ankitkushwah07022003@gmail.com>
2025-10-25 22:35:29 +0530

    change all file

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 
 app.use(cors({
-   origin: ["http://localhost:3000" ,"http://localhost:3001"],
+   origin: ["https://zerodha-1-frontend-xgzy.onrender.com" ,"http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
